a781917a438c9135345a08ea6493d9044e1cd9e4
venkat-7788 <venkatsaisharan@gmail.com>
2024-05-09 19:54:26 +1000

    Upload SQL files

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -13,32 +13,32 @@ NOTE: If you are having issues with permissions. And you get error:
     1. Find path by right-clicking a CSV file in VS Code and selecting “Copy Path”
 5. Paste the following into `PSQL Tool`, (with the CORRECT file path)
 
-\copy company_dim FROM '/Users/venkatasaisharan/Downloads/SQL_Project_data_for_Job_analysis/csv_files/company_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy company_dim FROM '/Users/venkatasaisharan/Downloads/SQL_Project_Data_Job_Analysis/csv_files/company_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-\copy skills_dim FROM '/Users/venkatasaisharan/Downloads/SQL_Project_data_for_Job_analysis/csv_files/skills_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy skills_dim FROM '/Users/venkatasaisharan/Downloads/SQL_Project_Data_Job_Analysis/csv_files/skills_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-\copy job_postings_fact FROM '/Users/venkatasaisharan/Downloads/SQL_Project_data_for_Job_analysis/csv_files/job_postings_fact.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy job_postings_fact FROM '/Users/venkatasaisharan/Downloads/SQL_Project_Data_Job_Analysis/csv_files/job_postings_fact.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-\copy skills_job_dim FROM '/Users/venkatasaisharan/Downloads/SQL_Project_data_for_Job_analysis/csv_files/skills_job_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy skills_job_dim FROM '/Users/venkatasaisharan/Downloads/SQL_Project_Data_Job_Analysis/csv_files/skills_job_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 */
 
 -- NOTE: This has been updated from the video to fix issues with encoding
 
 COPY company_dim
-FROM '/Users/venkatasaisharan/Downloads/SQL_Project_data_for_Job_analysis/csv_files/company_dim.csv'
+FROM '/Users/venkatasaisharan/Downloads/SQL_Project_Data_Job_Analysis/csv_files/company_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COPY skills_dim
-FROM '/Users/venkatasaisharan/Downloads/SQL_Project_data_for_Job_analysis/csv_files/skills_dim.csv'
+FROM '/Users/venkatasaisharan/Downloads/SQL_Project_Data_Job_Analysis/csv_files/skills_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COPY job_postings_fact
-FROM '/Users/venkatasaisharan/Downloads/SQL_Project_data_for_Job_analysis/csv_files/job_postings_fact.csv'
+FROM '/Users/venkatasaisharan/Downloads/SQL_Project_Data_Job_Analysis/csv_files/job_postings_fact.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COPY skills_job_dim
-FROM '/Users/venkatasaisharan/Downloads/SQL_Project_data_for_Job_analysis/csv_files/skills_job_dim.csv'
+FROM '/Users/venkatasaisharan/Downloads/SQL_Project_Data_Job_Analysis/csv_files/skills_job_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 
